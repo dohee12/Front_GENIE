d82b03620695e45c4ce84fedd464b6f32743ee3d
dohee12 <happyddo42@gmail.com>
2025-01-12 00:02:35 +0900

    feat: 회원가입, 아이디 찾기, 비밀번호 찾기 라우터를 사용하여 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LoginBox from './components/LoginBox';
+import SignupBox from './components/SignupBox';
 import Footer from './components/Footer';
+import FindIdBox from './components/FindIdBox';
+import FindPwBox from './components/FindPwBox';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header/>
       <main>
-        <LoginBox/>
+        <Routes>
+          <Route path="/find-password" element={<FindPwBox />} />
+          <Route path='/find-id' element={<FindIdBox />} />
+          <Route path="/signup" element={<SignupBox />} />
+          <Route path="/login" element={<LoginBox />} />
+        </Routes>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 };
 
