5fccfcf908f4c0becd0731496adc074cc401bf91
dohee12 <happyddo42@gmail.com>
2025-01-12 00:03:48 +0900

    feat: 푸터 구현

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='py-4 text-sm text-center font-black font-bold border-t'>
+        <footer className='py-4 text-sm text-center font-bold border-t bg-gray-100'>
             <p>
-                <a href="/terms" className='hover:underline'>이용약관 | </a>
-                <a href='/privacy' className='hover:underLine'>개인정보처리방침 | </a>
-                <a href='/contact' className='hover:overline'>이메일주소무단수집거부 | </a>
+                <a href="/terms" className='hover:underline'>이용약관</a> | 
+                <a href='/privacy' className='hover:underline'>개인정보처리방침</a> | 
+                <a href='/contact' className='hover:underline'>이메일주소무단수집거부</a> | 
                 <a href='/report' className='hover:underline'>고객센터</a>
             </p>
-            <p className='mt-2'>Copyright@GENIE MUSIC Corp.All rights reserved.</p>
+            <p className='mt-2'>Copyright © GENIE MUSIC Corp. All rights reserved.</p>
         </footer>
     );
 };
