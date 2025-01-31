1a6fa584d158539bdbcb2b43fe1bd6aad5659377
dohee12 <happyddo42@gmail.com>
2025-01-31 21:26:54 +0900

    feat: 헤더 디자인 수정 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Logo from './../img/genieLogo.png';
+import React from "react";
+import Logo from "./../img/genieLogo.png";
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between px-8 py-4 border-b bg-white">
-            <a href='/login'><img src= {Logo} alt='지니뮤직 로고' className='w-20 h-10'/></a>
-            <nav className='space-x-6 text-sm text-gray-600'>
-                <a href="/signup">회원가입</a>
-                <a href="/find-id">아이디 찾기</a>
-                <a href="/find-password">비밀번호 찾기</a>
-                <a href="/login" className='text-blue-600 font-bold'>로그인</a>
+        <header className="flex items-center justify-between h-16 px-56 py-3 border-b bg-white">
+            <a href="/login">
+                <img src={Logo} alt="지니뮤직 로고" className="w-28"/>
+            </a>
+            <nav className="flex space-x-8 text-gray-600 text-sm">
+                <a href="/signup" className="hover:text-blue-600">회원가입</a>
+                <a href="/find-id" className="hover:text-blue-600">아이디 찾기</a>
+                <a href="/find-password" className="hover:text-blue-600">비밀번호 찾기</a>
+                <a href="/login" className="text-blue-500 font-bold">로그인</a>
             </nav>
         </header>
-    );
-};
+    )
+}
 
 export default Header;
